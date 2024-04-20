7051a31a312328adbb5d9dd26938c431f43c736e
Philip Karanja <kpk.my.mailings@gmail.com>
2024-04-20 10:53:07 -0500

    temp

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     const container = document.getElementById("container");
     const createLabel = (name) => {
-      
+      const label = document.createElement("div");
+  
     }
   }
