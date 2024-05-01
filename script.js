d9096df5e9e15486f37c54fa2fd940728bda014d
Philip Karanja <kpk.my.mailings@gmail.com>
2024-05-01 16:46:48 -0500

    applyingFunction function

--- script.js
+++ script.js
@@ -38,10 +38,20 @@ const median = nums => {
 }
 //using shorthand property to declare an object literal spredsheetFunctions is the object with properties sum, average and median
 
-const spreadSheetFunctions = {
+const spreadsheetFunctions = {
     sum,
     average,
     median
+}
+
+const applyFunction = (str) => {
+  const noHigh = highPrecedence(str);
+  const infix =  /([\d.]+)([+-])([\d.]+)/
+  const str2 = infixEval(noHigh, infix);
+  const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+  const toNumberList = (args) => args.split(",").map(parseFloat);
+  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+  return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn, args) : match);
 }
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
