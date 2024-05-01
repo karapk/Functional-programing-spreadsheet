de53212542da17eaa0ad385ba666eabf4e592ccf
Philip Karanja <kpk.my.mailings@gmail.com>
2024-05-01 00:36:36 -0500

    finishing division and multiplication

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const infixToFunction = {
   "*": (x, y) => x * y,
   "/": (x, y) => x / y,
 };
+//evaluating the functions
+const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
+
+//regular expression that matches a number including a decimal followed by * or / operator followed by another number
+const highPrecedence = (str) => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
+  return str === str2 ? str : highPrecedence(str2);
+};
+
 //isEven function takes num as parameter and returns true if number is even and false if not. The === is the strictly equal operator
 const isEven = (num) => num % 2 === 0;
 
